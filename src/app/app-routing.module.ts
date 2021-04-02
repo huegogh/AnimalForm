1936697647e88925b3c31924c5a29c5605ce1d12
huegogh <huegogh42@gmail.com>
2021-04-02 16:15:22 -0700

    create animal form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './form/form.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path : "", component : LoginComponent},
+  {path : "form", component : FormComponent},
   {path : "**", redirectTo: ""},
 ];
 
