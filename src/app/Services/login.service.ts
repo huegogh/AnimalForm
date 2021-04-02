60fe69b8d386cfc0f4ef949efd7a8b099bee0681
huegogh <huegogh42@gmail.com>
2021-04-02 15:26:51 -0700

    add functionality to form

---
+++ src/app/Services/login.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { ILogin } from '../Interfaces/login';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  private Url = 'http://localhost:5000/auth/login';
+
+  private Login: ILogin = {
+    username: '',
+    password: ''
+  };
+
+  constructor(private http:HttpClient, private router:Router) {}
+
+  SignIn(loginData: ILogin){
+    console.log(loginData);  
+    this.http.post(this.Url, loginData)
+      .subscribe(response => {
+        console.log(response);
+        this.router.navigate(["home"]);
+      });
+  }
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from '../Services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  LoginForm!: FormGroup;
+
+  constructor(private form: FormBuilder, private loginService: LoginService) { }
 
   ngOnInit(): void {
+    this.LoginForm = this.form.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  submitForm() {
+    console.log('running submit');
+    this.loginService.SignIn(this.LoginForm.value);
+  }
+
+  Alert() {
+    alert("Why would you click that?! How will you open up the window if it were to close?");
   }
 
 }
